09ff04599a00f94ca79fa89a462fd360db7293d9
Aavesh Saifi <Saifiaavesh01@gmail.com>
2025-04-13 03:01:21 +0530

    feat: day1 rust repo

---
+++ day1/target/debug/day1.d
@@ -0,0 +1,1 @@
+D:\rust\day1\target\debug\day1.exe: D:\rust\day1\src\main.rs
---
+++ day1/target/debug/deps/day1.d
@@ -0,0 +1,5 @@
+D:\rust\day1\target\debug\deps\day1.exe: src\main.rs
+
+D:\rust\day1\target\debug\deps\day1.d: src\main.rs
+
+src\main.rs:
